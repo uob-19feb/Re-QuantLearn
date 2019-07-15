afff55dbac1b67302e01f4983610df1bd36e1232
uttam thapa <uttammoagar@gmail.com>
2019-07-15 14:55:22 +0545

    application is simplified and refactoring is almost complete. For further refactoring could be done when migrating the application

---
+++ appmain.js
@@ -0,0 +1,45 @@
+//importing moduels and api
+const { app, ipcMain, dailog, Menu, BrowserWindow } = require('electron')
+
+const path = require('path')
+const url  = require('url')
+const iconSrc = path.join(__dirname, '/resources/assets/icons/quantLearn.png')
+
+let mainWin = null
+
+//application closing 
+app.on("window-all-closed", () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+//window specificiation 
+app.on('ready', ()=>{
+  mainWin = new BrowserWindow({
+    'minWidth':900,
+    'minHeight':700,
+    width:1024,
+    height:815,
+    title:'QuantLearn',
+    // titleBarStyle: 'hiddenInset',
+    icon: iconSrc
+  })
+
+  mainWin.loadURL(url.format({
+    pathname: path.join(__dirname, '/resources/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  //dev tool 
+  mainWin.webContents.openDevTools()
+
+})
+
+if(process.platform === 'darwin'){
+  app.dock.setIcon(path.join(__dirname, '/resources/assets/icons/quantLearn.png'))
+}
+
+
+
--- main.js
+++ main.js
@@ -1,33 +1,52 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
+const menu
+const ipcMain 
+const dialog
+
+
+
+//my main coding started here
+app.on("window-all-closed", ()=> {
+  app.quit()
+})
+//this is to open a window
+app.on('ready', function appReady() {
+  mainWindow = new BrowserWindow({
+    'minWidth': 800,
+    'minHeight': 600,
+    width: 980,
+    height: 760,
+    title: 'Git-it',
+    icon: iconPath
+  })
+
+
+
+
+const iconPath = path.join(__dirname, '/resource/')
+let mainWin
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({
+  mainWin = new BrowserWindow({
     width: 800,
     height: 600,
+
+
     webPreferences: {
       nodeIntegration: true
     }
   })
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  mainWin.loadFile('index.html')
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  mainWin.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
-}
+
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
